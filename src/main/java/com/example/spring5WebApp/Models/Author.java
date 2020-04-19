cccbbacf2d0ba5375f6a50f029fe48a0ee637ef8
Zachary Melton <zac.melton@outlook.com>
2020-04-19 11:44:58 -0500

    Set up the author and book entities to have a many to many relationship

--- src/main/java/com/example/spring5WebApp/Models/Author.java
+++ src/main/java/com/example/spring5WebApp/Models/Author.java
@@ -1,20 +1,43 @@
 package com.example.spring5WebApp.Models;
 
 import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String firstName;
     private String lastName;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+        inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Book> books;
 
-    public Author() {
-        
-    }
+    public Author() {}
     
     public Author(String firstName, String lastName, Set<Book> books) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
     
     public Set<Book> getBooks() {
--- src/main/java/com/example/spring5WebApp/Models/Book.java
+++ src/main/java/com/example/spring5WebApp/Models/Book.java
@@ -1,19 +1,38 @@
 package com.example.spring5WebApp.Models;
 
 import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private String title;
     private String isbn;
+
+    @ManyToMany(mappedBy = "authors")
     private Set<Author> authors;
 
-    public Book() {
-        
-    }
+    public Book() {}
 
     public Book(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public Set<Author> getAuthors() {
